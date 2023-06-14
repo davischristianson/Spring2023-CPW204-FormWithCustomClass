b09929a59d77e9a86c843c2c5b124c0843480f6e
davischristianson <dav.christian10@gmail.com>
2023-06-13 20:59:19 -0700

    Displayed added games on webpage

--- js/main.js
+++ js/main.js
@@ -29,4 +29,22 @@ function getVideoGame() {
     return game;
 }
 function displayGame(myGame) {
+    var displayDiv = document.getElementById("display");
+    var gameHeading = document.createElement("h2");
+    gameHeading.innerText = myGame.title;
+    var gameInfo = document.createElement("p");
+    var gameMediumDisplay = "";
+    if (myGame.isDigitalOnly) {
+        gameMediumDisplay = "This is a digital only game.";
+    }
+    else {
+        gameMediumDisplay = "You can buy a physical copy.";
+    }
+    gameInfo.innerText = myGame.title + " has a rating of " +
+        myGame.rating + ". It costs " + myGame.price +
+        ". It is " + gameMediumDisplay + " digital only";
+    gameInfo.innerText = "".concat(myGame.title, " has a rating of ").concat(myGame.rating, ". ") +
+        "It costs $".concat(myGame.price.toFixed(2), ". ").concat(gameMediumDisplay);
+    displayDiv.appendChild(gameHeading);
+    displayDiv.appendChild(gameInfo);
 }
--- ts/main.ts
+++ ts/main.ts
@@ -60,5 +60,30 @@ function getVideoGame():VideoGame {
 }
 
 function displayGame(myGame:VideoGame):void {
-    // TODO: Display video game below the form
+    let displayDiv = document.getElementById("display");
+
+    // Create <h2> with game title
+    let gameHeading = document.createElement("h2");
+    gameHeading.innerText = myGame.title;
+
+    // Create paragraph with game details
+    let gameInfo = document.createElement("p");
+    let gameMediumDisplay = "";
+    if(myGame.isDigitalOnly) {
+        gameMediumDisplay = "This is a digital only game.";
+    }
+    else {
+        gameMediumDisplay = "You can buy a physical copy."
+    }
+    gameInfo.innerText = myGame.title + " has a rating of " +
+                    myGame.rating + ". It costs " + myGame.price +
+                    ". It is " + gameMediumDisplay + " digital only";
+
+    gameInfo.innerText = `${myGame.title} has a rating of ${myGame.rating}. ` + 
+                        `It costs $${myGame.price.toFixed(2)}. ${gameMediumDisplay}`
+
+    // Add <h2> in the <div id="display">
+    displayDiv.appendChild(gameHeading);
+    // Add <p> game info
+    displayDiv.appendChild(gameInfo);
 }

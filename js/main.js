5f0b526f854d077ba4e0f4409b3d8b5fafa86529
davischristianson <dav.christian10@gmail.com>
2023-06-13 21:56:17 -0700

    Added validation for inputs, extra <h3> displaydiv

--- js/main.js
+++ js/main.js
@@ -8,13 +8,53 @@ window.onload = function () {
     addBtn.onclick = addVideoGame;
 };
 function addVideoGame() {
+    removeErrors();
     if (isAllDataValid()) {
         var game = getVideoGame();
         displayGame(game);
     }
 }
 function isAllDataValid() {
-    return true;
+    var titleInput = document.getElementById("title");
+    var titleString = titleInput.value;
+    var titleError = document.getElementById("title-error");
+    var priceInput = document.getElementById("price");
+    var priceNumber = parseFloat(priceInput.value);
+    var priceError = document.getElementById("price-error");
+    var descInput = document.getElementById("description");
+    var descString = descInput.value;
+    var descError = document.getElementById("description-error");
+    var ratingInput = document.getElementById("rating");
+    var ratingString = ratingInput.value;
+    var ratingError = document.getElementById("rating-error");
+    var dataValid = true;
+    if (titleString.trim() == "") {
+        titleError.innerText = "Enter a valid game title";
+        dataValid = false;
+    }
+    if (isNaN(priceNumber)) {
+        priceError.innerText = "Enter a valid numeric price";
+        dataValid = false;
+    }
+    if (descString.trim() == "") {
+        descError.innerText = "Enter a valid description of the game";
+        dataValid = false;
+    }
+    if (ratingString == "Please choose a rating") {
+        ratingError.innerText = "Pick a rating for the game";
+        dataValid = false;
+    }
+    return dataValid;
+}
+function removeErrors() {
+    var titleError = document.getElementById("title-error");
+    var priceError = document.getElementById("price-error");
+    var descriptionError = document.getElementById("description-error");
+    var ratingError = document.getElementById("rating-error");
+    titleError.innerText = "*";
+    priceError.innerText = "*";
+    descriptionError.innerText = "*";
+    ratingError.innerText = "*";
 }
 function getVideoGame() {
     var game = new VideoGame();
@@ -22,6 +62,8 @@ function getVideoGame() {
     game.title = titleInput.value;
     var priceInput = document.getElementById("price");
     game.price = parseFloat(priceInput.value);
+    var descriptionInput = document.getElementById("description");
+    game.description = descriptionInput.value;
     var ratingInput = document.getElementById("rating");
     game.rating = ratingInput.value;
     var digitalOnly = document.getElementById("online");
@@ -45,6 +87,9 @@ function displayGame(myGame) {
         ". It is " + gameMediumDisplay + " digital only";
     gameInfo.innerText = "".concat(myGame.title, " has a rating of ").concat(myGame.rating, ". ") +
         "It costs $".concat(myGame.price.toFixed(2), ". ").concat(gameMediumDisplay);
+    var gameDesc = document.createElement("h3");
+    gameDesc.innerText = myGame.description;
     displayDiv.appendChild(gameHeading);
+    displayDiv.appendChild(gameDesc);
     displayDiv.appendChild(gameInfo);
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,6 +1,7 @@
 class VideoGame {
     title:string;
     price:number;
+    description:string;
     rating:string;
     isDigitalOnly:boolean;
 }
@@ -17,6 +18,7 @@ window.onload = function() {
 }
 
 function addVideoGame() {
+    removeErrors();
     if(isAllDataValid()) {
         let game = getVideoGame();
         displayGame(game);
@@ -24,7 +26,57 @@ function addVideoGame() {
 }
 
 function isAllDataValid() {
-    return true;
+    let titleInput = <HTMLInputElement>document.getElementById("title");
+    let titleString:string = titleInput.value;
+    let titleError = document.getElementById("title-error");
+
+    let priceInput = <HTMLInputElement>document.getElementById("price");
+    let priceNumber:number = parseFloat(priceInput.value);
+    let priceError = document.getElementById("price-error");
+    
+    let descInput = <HTMLInputElement>document.getElementById("description");
+    let descString:string = descInput.value;
+    let descError = document.getElementById("description-error");
+
+    let ratingInput = <HTMLSelectElement>document.getElementById("rating"); 
+    let ratingString:string = ratingInput.value;
+    let ratingError = document.getElementById("rating-error");
+
+    let dataValid = true;
+
+    if(titleString.trim() == "") {
+        titleError.innerText = "Enter a valid game title";
+        dataValid = false;
+    }
+
+    if(isNaN(priceNumber)) {
+        priceError.innerText = "Enter a valid numeric price";
+        dataValid = false;
+    }
+
+    if(descString.trim() == "") {
+        descError.innerText = "Enter a valid description of the game";
+        dataValid = false;
+    }
+
+    if(ratingString == "Please choose a rating") {
+        ratingError.innerText = "Pick a rating for the game";
+        dataValid = false;
+    }
+    return dataValid;
+}
+
+function removeErrors() {
+
+    let titleError = document.getElementById("title-error");
+    let priceError = document.getElementById("price-error");
+    let descriptionError = document.getElementById("description-error");
+    let ratingError = document.getElementById("rating-error");
+
+    titleError.innerText = "*";
+    priceError.innerText = "*";
+    descriptionError.innerText = "*";
+    ratingError.innerText = "*";
 }
 
 /**
@@ -35,11 +87,14 @@ function isAllDataValid() {
 function getVideoGame():VideoGame {
     let game = new VideoGame();
     // TODO: Populate with data from the form
-    let titleInput = <HTMLInputElement>document.getElementById("title")
+    let titleInput = <HTMLInputElement>document.getElementById("title");
     game.title = titleInput.value;
 
     let priceInput = <HTMLInputElement>document.getElementById("price");
     game.price = parseFloat(priceInput.value);
+
+    let descriptionInput = <HTMLInputElement>document.getElementById("description");
+    game.description = descriptionInput.value;
 
     let ratingInput = <HTMLSelectElement>document.getElementById("rating"); 
     game.rating = ratingInput.value;
@@ -82,8 +137,15 @@ function displayGame(myGame:VideoGame):void {
     gameInfo.innerText = `${myGame.title} has a rating of ${myGame.rating}. ` + 
                         `It costs $${myGame.price.toFixed(2)}. ${gameMediumDisplay}`
 
+                        
+    // Create <h3> that gives a game description
+    let gameDesc = document.createElement("h3");
+    gameDesc.innerText = myGame.description;
+
     // Add <h2> in the <div id="display">
     displayDiv.appendChild(gameHeading);
+    // Add <h3> game description
+    displayDiv.appendChild(gameDesc);
     // Add <p> game info
     displayDiv.appendChild(gameInfo);
 }
